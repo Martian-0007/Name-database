fbe7fc63163c61b97709662617eb5036f84d2a67
Martian-0007 <81148675+Martian-0007@users.noreply.github.com>
2022-08-12 20:05:06 +0200

    pravopis

--- name_collect_functions.py
+++ name_collect_functions.py
@@ -1,7 +1,7 @@
 def getWriteName(filename):
     #Gets the name of a person and writes it into a file
     f = open(filename, "a")
-    name = input("Jak se jmenuje?\n")
+    name = input("Jak se jmenuješ?\n")
     f.write(name+"\n")
     f.close()
 
