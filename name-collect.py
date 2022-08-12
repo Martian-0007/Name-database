67a8cbecd929c3c8da1bd17f3547244bc6bc1fd7
Martian-0007 <81148675+Martian-0007@users.noreply.github.com>
2022-08-12 18:44:50 +0200

    Pridan vypis a odstraneni stareho souboru
    Pridano vypisovani ucastniku na konci a odstraneni stareho souboru

--- name-collect.py
+++ name-collect.py
@@ -1,5 +1,6 @@
-# Import of mine module
+# Import of modules
 import name_collect_functions as fx
+import os
 
 # Variables
 GN = input("Kolik je účastníků?\n")
@@ -7,8 +8,23 @@ GN = int(GN)
 
 file = "names.txt"
 
+#deleting the old file, if there is one
+try:
+    os.remove(file)
+except FileNotFoundError:
+    print("-")
 
 # The code itself
+# Getting some space after the Guest number question
+print()
+
 for i in range(GN):
+    # Write of the names into the file
     fx.flwrite(file, str(i+1)+"# ")
     fx.getWriteName(file)
+    print()
+
+# Print out the file full of names
+f = open(file, "r")
+print("\nVýpis účastníků:")
+print("\n"+f.read())
