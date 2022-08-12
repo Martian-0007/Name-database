714bdb95043f126c6bca2f510531cc6322ecdf0c
Martian-0007 <81148675+Martian-0007@users.noreply.github.com>
2022-08-12 19:59:16 +0200

    Time in the filename and not requiring guste count

--- name-collect.py
+++ name-collect.py
@@ -1,30 +1,35 @@
 # Import of modules
+from datetime import datetime
 import name_collect_functions as fx
 import os
+from datetime import datetime
 
 # Variables
-GN = input("Kolik je účastníků?\n")
-GN = int(GN)
-
-file = "names.txt"
-
-#deleting the old file, if there is one
-try:
-    os.remove(file)
-except FileNotFoundError:
-    print("-")
+now = datetime.now()
+current_time = now.strftime("%d-%m-%Y_%H.%M.%S")
+file = "C://Users/marta/Documents/GitHub/Name-database/files/names-"+current_time+".txt"
+more = True
+i = 1
 
 # The code itself
-# Getting some space after the Guest number question
-print()
-
-for i in range(GN):
+while(more): #WIP here
     # Write of the names into the file
-    fx.flwrite(file, str(i+1)+"# ")
+    fx.flwrite(file, str(i)+"# ")
     fx.getWriteName(file)
     print()
+    morein = input("Ještě někdo? [Y/n]")
+    if morein != "Y":
+        more = False
+#    if morein == "n":
+#        more = False
+#    elif morein != "Y":
+#        print("Tato odpověď nebyla v nabídce - konec")
+#        quit()
+    print()
+    i = i+1
 
 # Print out the file full of names
 f = open(file, "r")
 print("\nVýpis účastníků:")
 print("\n"+f.read())
+f.close
