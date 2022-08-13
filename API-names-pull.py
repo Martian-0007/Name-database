12591adb917498734a36ac4ffc9b98106e8b1fa0
Martian-0007 <81148675+Martian-0007@users.noreply.github.com>
2022-08-13 13:16:07 +0200

    Pavopis

--- API-names-pull.py
+++ API-names-pull.py
@@ -4,7 +4,7 @@ import json
 from datetime import datetime
 
 # Variables
-filename = input("Jak0 je jmeno souboru ke stazeni z API?\n")
+filename = input("Jaké je jméno souboru ke stažení z API?\n")
 url = "https://getpantry.cloud/apiv1/pantry/fbd15934-f1bf-4621-9086-e28c9e1c3b7c/basket/"+filename
 now = datetime.now()
 current_time = now.strftime("%d-%m-%Y_%H.%M.%S")
